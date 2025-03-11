0137b6ea7e7ef3efb40189a6a14130a19c102d89
Pedro de Oliveira Santos <158167681+pedro-ols@users.noreply.github.com>
2025-03-11 09:33:28 -0300

    feat: create sidebar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 import styles from './page.module.css';
 import Header from '../components/header'
+import SideBar from '../components/sidebar';
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <main classNane={styles.dashboardContainer}>
+      <SideBar/>
+      </main>
     </div>
   );
 }
